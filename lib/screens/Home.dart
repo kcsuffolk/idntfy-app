449ef3fca195ec0b6c9ad88c723f6081a7162c7f
Kevin Suffolk <kevinsuffolk@Kevins-MacBook-Pro.local>
2020-01-15 14:47:34 +0100

    Improvements Home & Profile Design

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:idntfy_app/widgets/LatestTransactions.dart';
+import 'package:idntfy_app/presentation/custom_icons_icons.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -27,38 +28,79 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Latest Transactions',
-          style: TextStyle(
-            color: Colors.black,
-            fontFamily: 'Poppins',
-            fontWeight: FontWeight.bold,
-            fontSize: 24,
-          ),
-        ),
-        elevation: 0,
-        backgroundColor: Colors.white,
-        brightness: Brightness.light,
-      ),
-      body: ListView.builder(
-        itemCount: lastTransaction.length,
-        itemBuilder: (context, index) {
-          return Container(
-            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
-            // child: Card(
-            child: ListTile(
-              onTap: () {},
-              title: Text(lastTransaction[index].provider),
-              subtitle: Text(lastTransaction[index].datapoints),
-              leading: CircleAvatar(
-                backgroundImage:
-                    AssetImage('images/logos/${lastTransaction[index].logo}'),
+      // appBar: AppBar(
+      //   title: Text(
+      //     'Latest Transactions',
+      //     style: TextStyle(
+      //       color: Colors.black,
+      //       fontFamily: 'Poppins',
+      //       fontWeight: FontWeight.bold,
+      //       fontSize: 24,
+      //     ),
+      //   ),
+      //   elevation: 0,
+      //   backgroundColor: Colors.white,
+      //   brightness: Brightness.light,
+      // ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                SizedBox(
+                  height: 50.0,
+                ),
+                IconButton(
+                  icon: Icon(CustomIcons.grid),
+                  iconSize: 18.0,
+                  onPressed: () {},
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20.0),
+                  child: IconButton(
+                    icon: Icon(CustomIcons.search),
+                    iconSize: 18.0,
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(
+                'Latest Transactions',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24.0,
+                ),
               ),
             ),
-            // ),
-          );
-        },
+            Expanded(
+              child: ListView.builder(
+                itemCount: lastTransaction.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 1.0, horizontal: 4.0),
+                    // child: Card(
+                    child: ListTile(
+                      onTap: () {},
+                      title: Text(lastTransaction[index].provider),
+                      subtitle: Text(lastTransaction[index].datapoints),
+                      leading: CircleAvatar(
+                        backgroundImage: AssetImage(
+                            'images/logos/${lastTransaction[index].logo}'),
+                      ),
+                    ),
+                    // ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/Profile.dart
+++ lib/screens/Profile.dart
@@ -9,76 +9,79 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Container(
-              child: Center(
-                child: Column(
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundImage: AssetImage('images/faces/face.png'),
-                      maxRadius: 25,
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(vertical: 15.0),
-                      child: Text(
-                        'Hanna Simons',
-                        style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 19.0),
-                      ),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
-                      margin: EdgeInsets.only(bottom: 30.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xff43D098), // Todo: Change Color
-                        borderRadius: BorderRadius.circular(22.0),
-                      ),
-                      child: Text(
-                        'Verified',
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Color(0xffF3FCF5),
             ),
-            Container(
-              margin: EdgeInsets.only(top: 25.0),
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 55.0),
+              child: Column(
                 children: <Widget>[
-                  Text('User Information'),
-                  Text('Sharing'),
+                  CircleAvatar(
+                    backgroundImage: AssetImage('images/faces/face.png'),
+                    maxRadius: 25,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 15.0),
+                    child: Text(
+                      'Hanna Simons',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 19.0),
+                    ),
+                  ),
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                    margin: EdgeInsets.only(bottom: 30.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xff43D098), // Todo: Change Color
+                      borderRadius: BorderRadius.circular(22.0),
+                    ),
+                    child: Text(
+                      'Verified',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
-            Container(
-                //   child: ListView.builder(
-                //     // itemCount: 2,
-                //     itemBuilder: (context, index) {
-                //       return Container(
-                //         padding: const EdgeInsets.symmetric(
-                //             vertical: 1.0, horizontal: 4.0),
-                //         child: ListTile(
-                //           onTap: () {},
-                //           title: Text('Placeholder'),
-                //           subtitle: Text('Placeholder'),
-                //         ),
-                //       );
-                //     },
-                //   ),
-                ),
-          ],
-        ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 25.0),
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('User Information'),
+                Text('Sharing'),
+              ],
+            ),
+          ),
+          Container(
+              //   child: ListView.builder(
+              //     // itemCount: 2,
+              //     itemBuilder: (context, index) {
+              //       return Container(
+              //         padding: const EdgeInsets.symmetric(
+              //             vertical: 1.0, horizontal: 4.0),
+              //         child: ListTile(
+              //           onTap: () {},
+              //           title: Text('Placeholder'),
+              //           subtitle: Text('Placeholder'),
+              //         ),
+              //       );
+              //     },
+              //   ),
+              ),
+        ],
       ),
     );
   }
