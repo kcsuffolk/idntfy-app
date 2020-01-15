6babd20ad810a0b975224616cb03908c66984193
Kevin Suffolk <kevinsuffolk@Kevins-MacBook-Pro.local>
2020-01-15 15:26:36 +0100

    Added EndDrawer

--- lib/screens/Profile.dart
+++ lib/screens/Profile.dart
@@ -6,81 +6,100 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      key: scaffoldKey,
+      endDrawer: Drawer(
+        child: ListView(),
+      ),
+      body: Stack(
         children: <Widget>[
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Color(0xffF3FCF5),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(top: 55.0),
-              child: Column(
-                children: <Widget>[
-                  CircleAvatar(
-                    backgroundImage: AssetImage('images/faces/face.png'),
-                    maxRadius: 25,
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 15.0),
-                    child: Text(
-                      'Hanna Simons',
-                      style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.bold,
-                          fontSize: 19.0),
-                    ),
-                  ),
-                  Container(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
-                    margin: EdgeInsets.only(bottom: 30.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xff43D098), // Todo: Change Color
-                      borderRadius: BorderRadius.circular(22.0),
-                    ),
-                    child: Text(
-                      'Verified',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        color: Colors.white,
+          Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Color(0xffF3FCF5),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 55.0),
+                  child: Column(
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundImage: AssetImage('images/faces/face.png'),
+                        maxRadius: 25,
                       ),
-                    ),
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 15.0),
+                        child: Text(
+                          'Hanna Simons',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 19.0),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 5.0),
+                        margin: EdgeInsets.only(bottom: 30.0),
+                        decoration: BoxDecoration(
+                          color: Color(0xff43D098), // Todo: Change Color
+                          borderRadius: BorderRadius.circular(22.0),
+                        ),
+                        child: Text(
+                          'Verified',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
+              Container(
+                margin: EdgeInsets.only(top: 25.0),
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('User Information'),
+                    Text('Sharing'),
+                  ],
+                ),
+              ),
+              Container(
+                  //   child: ListView.builder(
+                  //     // itemCount: 2,
+                  //     itemBuilder: (context, index) {
+                  //       return Container(
+                  //         padding: const EdgeInsets.symmetric(
+                  //             vertical: 1.0, horizontal: 4.0),
+                  //         child: ListTile(
+                  //           onTap: () {},
+                  //           title: Text('Placeholder'),
+                  //           subtitle: Text('Placeholder'),
+                  //         ),
+                  //       );
+                  //     },
+                  //   ),
+                  ),
+            ],
+          ),
+          Positioned(
+            right: 20,
+            top: 55,
+            child: IconButton(
+              icon: Icon(Icons.menu),
+              color: Colors.black,
+              onPressed: () => scaffoldKey.currentState.openEndDrawer(),
             ),
           ),
-          Container(
-            margin: EdgeInsets.only(top: 25.0),
-            padding: EdgeInsets.symmetric(horizontal: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('User Information'),
-                Text('Sharing'),
-              ],
-            ),
-          ),
-          Container(
-              //   child: ListView.builder(
-              //     // itemCount: 2,
-              //     itemBuilder: (context, index) {
-              //       return Container(
-              //         padding: const EdgeInsets.symmetric(
-              //             vertical: 1.0, horizontal: 4.0),
-              //         child: ListTile(
-              //           onTap: () {},
-              //           title: Text('Placeholder'),
-              //           subtitle: Text('Placeholder'),
-              //         ),
-              //       );
-              //     },
-              //   ),
-              ),
         ],
       ),
     );
