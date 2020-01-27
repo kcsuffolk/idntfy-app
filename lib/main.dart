d2d5bf9cd6bb4be32e04b7f77662e747919a9795
Kevin Suffolk <kevinsuffolk@Kevins-MacBook-Pro.local>
2020-01-27 12:48:10 +0100

    File Restructure

--- lib/main.dart
+++ lib/main.dart
@@ -10,49 +10,18 @@ import 'package:idntfy_app/screens/scan/qr_scan.dart';
 import 'package:idntfy_app/screens/home/home.dart';
 import 'package:idntfy_app/screens/wrapper.dart';
 import 'package:idntfy_app/services/auth.dart';
+import 'package:idntfy_app/shared/app_theme.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamProvider<User>.value(
       value: AuthService().user,
       child: MaterialApp(
-        theme: ThemeData(
-          primaryColor: Colors.white,
-          scaffoldBackgroundColor: Colors.white,
-          fontFamily: 'Inter',
-          textTheme: TextTheme(
-            title: TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: 26.0,
-              fontWeight: FontWeight.bold,
-              color: Color(0xFF101D15),
-            ),
-            subtitle: TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: 19.0,
-              fontWeight: FontWeight.w600,
-              color: Color(0xFF101D15),
-            ),
-            subhead: TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.w600,
-              color: Color(0xFF101D15),
-            ),
-            display1: TextStyle(
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.bold,
-              color: Color(0xFF101D15),
-            ),
-            body1: TextStyle(
-              fontSize: 16.0,
-              color: Color(0xFF3F4441),
-            ),
-          ),
-        ),
+        theme: appTheme(),
         initialRoute: './',
         routes: {
           './': (context) => Wrapper(),
