ea7f9a36430559df43af9b16384a5d39f07c5649
Kevin Suffolk <kevinsuffolk@Kevins-MacBook-Pro.local>
2020-01-27 18:33:13 +0100

    User Restructure

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,18 +1,12 @@
 class User {
   final String uid;
-
-  User({this.uid});
-}
-
-class UserData {
-  final String uid;
   final String name;
   final String address;
   final String email;
   final String phoneNumber;
   final String creditCard;
 
-  UserData({
+  User({
     this.uid,
     this.name,
     this.address,
