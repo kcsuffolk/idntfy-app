63fdafbb683d08980cfc59ea46d140d8c8420937
Kevin Suffolk <kevinsuffolk@Kevins-MacBook-Pro.local>
2020-02-23 20:24:20 +0100

    Performance Fix

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -16,58 +16,55 @@ class Profile extends StatelessWidget {
     return StreamBuilder<UserData>(
         stream: DatabaseService(uid: userAuthStream.uid).getUserDocuments,
         builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            UserData userData = snapshot.data;
+          if (!snapshot.hasData) return Loading();
+          UserData userData = snapshot.data;
 
-            return Scaffold(
-              backgroundColor: Colors.white,
-              body: Column(
-                children: <Widget>[
-                  SizedBox(height: 55.0),
-                  CircleAvatar(
-                    backgroundImage: AssetImage('images/faces/face.png'),
-                    radius: 35.0,
-                  ),
-                  SizedBox(height: 15.0),
-                  Text(
-                    userData.name,
-                    style: Theme.of(context).textTheme.subtitle,
-                  ),
-                  SizedBox(height: 15.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.verified_user,
-                        color: Color(0xff43D098),
-                      ),
-                      SizedBox(width: 5.0),
-                      Text(
-                        'verified',
-                        style: TextStyle(
-                            fontFamily: 'Poppins', color: Colors.black),
-                      )
-                    ],
-                  ),
-                  Expanded(
-                    child: ProfileList(),
-                  ),
-                  FlatButton.icon(
-                    icon: Icon(
-                      Icons.highlight_off,
-                      color: Colors.red,
+          return Scaffold(
+            backgroundColor: Colors.white,
+            body: Column(
+              children: <Widget>[
+                SizedBox(height: 55.0),
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/faces/face.png'),
+                  radius: 35.0,
+                ),
+                SizedBox(height: 15.0),
+                Text(
+                  userData.name,
+                  style: Theme.of(context).textTheme.subtitle,
+                ),
+                SizedBox(height: 15.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.verified_user,
+                      color: Color(0xff43D098),
                     ),
-                    label: Text('Sign out'),
-                    onPressed: () async {
-                      await _auth.signOut();
-                    },
+                    SizedBox(width: 5.0),
+                    Text(
+                      'verified',
+                      style:
+                          TextStyle(fontFamily: 'Poppins', color: Colors.black),
+                    )
+                  ],
+                ),
+                Expanded(
+                  child: ProfileList(),
+                ),
+                FlatButton.icon(
+                  icon: Icon(
+                    Icons.highlight_off,
+                    color: Colors.red,
                   ),
-                ],
-              ),
-            );
-          } else {
-            return Loading();
-          }
+                  label: Text('Sign out'),
+                  onPressed: () async {
+                    await _auth.signOut();
+                  },
+                ),
+              ],
+            ),
+          );
         });
   }
 }
