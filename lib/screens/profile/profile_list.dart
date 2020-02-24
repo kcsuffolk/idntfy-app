ba47290d75963e174b6d0e3d0a08addc68ec5349
Kevin Suffolk <kevinsuffolk@Kevins-MacBook-Pro.local>
2020-02-24 17:17:23 +0100

    Profile List Performance Update

--- lib/screens/profile/profile_list.dart
+++ lib/screens/profile/profile_list.dart
@@ -13,31 +13,28 @@ class ProfileList extends StatelessWidget {
     return StreamBuilder<UserData>(
         stream: DatabaseService(uid: userAuthStream.uid).getUserDocuments,
         builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            UserData userData = snapshot.data;
+          if (!snapshot.hasData) return Loading();
+          UserData userData = snapshot.data;
 
-            return ListView.separated(
-              itemCount: userData.toMap().length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(userData.toMap().keys.toList()[index],
-                      style: Theme.of(context).textTheme.subhead),
-                  subtitle: Text(userData.toMap().values.toList()[index],
-                      style: TextStyle(height: 2.0)),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => ProfileEdit()));
-                  },
-                  trailing: Icon(Icons.arrow_forward_ios),
-                );
-              },
-              separatorBuilder: (BuildContext context, index) => Divider(
-                thickness: 1,
-              ),
-            );
-          } else {
-            return Loading();
-          }
+          return ListView.separated(
+            itemCount: userData.toMap().length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(userData.toMap().keys.toList()[index],
+                    style: Theme.of(context).textTheme.subhead),
+                subtitle: Text(userData.toMap().values.toList()[index],
+                    style: TextStyle(height: 2.0)),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ProfileEdit()));
+                },
+                trailing: Icon(Icons.arrow_forward_ios),
+              );
+            },
+            separatorBuilder: (BuildContext context, index) => Divider(
+              thickness: 1,
+            ),
+          );
         });
   }
 }
