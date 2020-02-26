d7f570e718b92aded7887832be606537af702982
Kevin Suffolk <kevinsuffolk@Kevins-MacBook-Pro.local>
2020-02-26 13:44:16 +0100

    Stream Fix Provider Access

--- lib/screens/providers/provider_access.dart
+++ lib/screens/providers/provider_access.dart
@@ -1,39 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:idntfy_app/models/user.dart';
 import 'package:idntfy_app/screens/providers/provider_access_list.dart';
+import 'package:idntfy_app/services/database.dart';
+import 'package:provider/provider.dart';
 
 class ProviderAccess extends StatelessWidget {
   final String providerID;
-  final String providerAccessRef;
 
-  ProviderAccess({Key key, this.providerID, this.providerAccessRef})
-      : super(key: key);
+  ProviderAccess({Key key, this.providerID});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios),
-          onPressed: () => Navigator.of(context).pop(),
+    final userAuthStream = Provider.of<UserData>(context);
+
+    return StreamProvider<List<UserData>>.value(
+      value: DatabaseService(uid: userAuthStream.uid, providerID: providerID)
+          .getProviderAccessCollection,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: Text(
+            'Provider Access',
+            style: Theme.of(context).textTheme.subtitle,
+          ),
         ),
-        title: Text(
-          'Provider Access',
-          style: Theme.of(context).textTheme.subtitle,
-        ),
-      ),
-      body: Column(
-        children: <Widget>[
-          ClipOval(
-            child: Image.asset(
-              'images/logos/${['logo']}',
-              width: 85.0,
+        body: Column(
+          children: <Widget>[
+            ClipOval(
+              child: Image.asset(
+                'images/logos/${['logo']}',
+                width: 85.0,
+              ),
             ),
-          ),
-          Expanded(
-            child: ProviderAccessList(providerID: providerID),
-          ),
-        ],
+            Expanded(
+              child: ProviderAccessList(),
+            ),
+          ],
+        ),
       ),
     );
   }
