30fe22d68bc575e5cb7df38905173b4624698260
Kevin Suffolk <kevinsuffolk@Kevins-MacBook-Pro.local>
2020-02-27 15:08:05 +0100

    Authorized Providers no Providers available

--- lib/screens/providers/provider_list.dart
+++ lib/screens/providers/provider_list.dart
@@ -8,11 +8,21 @@ class ProviderList extends StatelessWidget {
   Widget build(BuildContext context) {
     final providers = Provider.of<List<ProviderData>>(context) ?? [];
 
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return ProviderListTile(provider: providers[index]);
-      },
-      itemCount: providers.length,
-    );
+    if (providers.length > 1) {
+      return ListView.builder(
+        itemBuilder: (context, index) {
+          return ProviderListTile(provider: providers[index]);
+        },
+        itemCount: providers.length,
+      );
+    } else {
+      return Container(
+        padding: EdgeInsets.only(left: 25.0),
+        child: Text(
+          'All providers that have access to your data will be displayed here',
+          style: TextStyle(height: 1.5),
+        ),
+      );
+    }
   }
 }
