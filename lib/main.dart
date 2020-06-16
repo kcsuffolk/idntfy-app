9ec4b83fc7dafaa343193dd305306b92c237a92a
Kevin Suffolk <kevin.suffolk@outlook.com>
2020-06-16 13:57:27 +0200

    Enabled Debug Banner & Wrapper Optimization

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class App extends StatelessWidget {
     return StreamProvider<UserData>.value(
       value: AuthService().userState,
       child: MaterialApp(
-        debugShowCheckedModeBanner: false, // Remove this line for productive
+        debugShowCheckedModeBanner: true, // Remove for production mode
         theme: appTheme(),
         initialRoute: '/',
         routes: {
